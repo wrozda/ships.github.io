638368040ffee3790c609effd4391a436ef67785
wrozda <monika.czerniawska1991@gmail.com>
2018-09-12 15:20:11 +0200

    board

--- code.js
+++ code.js
@@ -1,20 +1,18 @@
 // piszemy kod do gry w statki: siatka = 6 , statek = 3//
 //deklarujemy zmienne przechowujące pola, pod którymi ukryty jest statek//
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
 
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 //deklaracja zmiennej przechowującej podane pole użytkownika //
 var guess;
-
 // deklaracja zmiennej z liczbą trafień //
-var hits = 0;
-
-// deklaracja zmiennej z liczbą prób //
+var hits = 0;// deklaracja zmiennej z liczbą prób //
 var guesses = 0;
-
 //deklaracja zmiennej z informacją o zatopieniu okrętu//
 var isSunk = false;
+
 
 //pętla - dopóki okręt nie jest zatopiony//
 while (isSunk == false) {
@@ -23,25 +21,24 @@ while (isSunk == false) {
         alert("Proszę podać rawidłowy numer komórki");
     } else {
         guesses = guesses + 1;
+
         if (guess == location1 || guess == location2 || guess == location3) {
             hits = hits + 1;
-
             alert('Trafiony!');
 
             if (hits == 3) {
                 isSunk = true;
-
                 alert('Zatopiłeś mój okręt!');
             }
         }else {
-            alert('Pudło...')
+            alert('Pudło...');
         }
 
     }
 }
 
 
-var stats = 'Potrzebowałeś ' + guesses + ' trafień, by zatopić okręt. <br> czyli Twoja efektywność wynosi 3/'+
-    guesses;
+var stats = 'Potrzebowałeś ' + guesses + ' trafień, by zatopić okręt. <br> czyli Twoja efektywność wynosi ' +
+    (3/guesses);
 
 alert(stats);
